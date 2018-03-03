825c4f0996cb89fc528044bb3f520787a22d0476
Varsha Amrutlal Katariya 012468079 <varshaamrutlal.katariya@sjsu.edu>
2018-03-03 02:13:20 -0800

    chnages in UI to call node and perform calculator operations

--- App.jsx
+++ App.jsx
@@ -4,65 +4,140 @@ import axios from 'axios';
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            result: 0
-        }
+        this.initialize();
         this.calculate = this.calculate.bind(this);
         this.clear = this.clear.bind(this);
-         //this.handleChange = this.handleChange.bind(this);
+        this.numberClicked = this.numberClicked.bind(this);
+        this.opClicked = this.opClicked.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     };
 
+    initialize(){
+        this.state = {
+            numberClicked:0,
+            number:[],
+            firstNumber:0,
+            secondNumber:0,
+            operator:'',
+            result: 0,
+            sign:'',
+            displayValue: 0
+        }
+    }
+
     handleChange(event) {
-        /*this.set.result = 0;
-        this.setState({
-            result:this.set.result
-        });*/
+        this.state.displayValue = event.target.value;
+        this.setState({displayValue: this.state.displayValue});
     }
 
     clear(){
-        document.getElementById("textbox1").value = 0;
+        this.initialize();
     }
 
     changeSign(){
 
     }
 
-    append(val){
-        var val = this.state.result + "" + val;
-
+    setFirstNumber(first) {
+        if (this.state.firstNumber == 0) {
+            this.initialize();
+            this.state.firstNumber = first;
+        }
+        else {
+            first = this.state.firstNumber + "" + first;
+        }
+        console.log("state first number : "+this.state.firstNumber);
+        this.state.firstNumber = first;
+        this.setState({
+            firstNumber: this.state.firstNumber
+        });
+        this.state.displayValue = this.state.firstNumber;
+        this.setState({
+            displayValue: this.state.displayValue
+        });
+        console.log("this.state.firstNumber "+this.state.firstNumber);
     }
 
-    numberClicked(value){
-        console.log("Number "+value);
+    setSecondNumber(sec){
+
+        if(this.state.secondNumber == 0 )
+            this.state.secondNumber = sec;
+        else {
+            sec = this.state.secondNumber + "" + sec;
+        }
+        console.log("state first number : "+this.state.secondNumber);
+
+        this.state.secondNumber = sec;
+        this.setState({
+            secondNumber: this.state.secondNumber
+        });
+        this.state.displayValue = this.state.secondNumber;
+        this.setState({
+            displayValue: this.state.displayValue
+        });
+        console.log("this.state.secondNumber "+this.state.secondNumber);
     }
 
-    opClicked(value){
-        console.log("Operator "+value);
+    numberClicked(event){
+
+        if(this.state.operator == ""){
+            this.setFirstNumber(event.target.value);
+        }
+        else{
+            this.setSecondNumber(event.target.value);
+        }
+    }
+
+    opClicked(event){
+        console.log("Operator " + event.target.value);
+
+        if(event.target.value == "="){
+            this.calculate();
+        }else {
+            if(event.target.value =="+")
+                this.state.operator = "add";
+            else if(event.target.value =="-")
+                this.state.operator = "sub";
+            else if(event.target.value =="*")
+                this.state.operator = "mult";
+            else if(event.target.value =="/")
+                this.state.operator = "div";
+            this.setState({
+                operator: this.state.operator
+            });
+        }
+
     }
 
     calculate(){
-        console.log(this);
-        /*console.log("subtraction "+document.getElementById("btnSub").value);
-          var n1 = Number(document.getElementById("textbox1").value);
-          var n2 = Number(document.getElementById("textbox2").value);
+        console.log(this.state.operator);
+
+          var n1 = Number(this.state.firstNumber);
+          var n2 = Number(this.state.secondNumber);
           var r = 0;
-          //var op = document.getElementById("opList").value;
+          var op = this.state.operator;
 
-         // console.log(document.getElementById("opList").value);
+         console.log(this.state.operator);
 
-          axios.get('http://localhost:3001/add', {params: { number1: n1, number2: n2 }
-              })
-                  .then(response => {
-                      this.state.result = response.data.result;
-                      this.setState({result:this.state.result});
-                      console.log(response.data.result, 'from Node jS!');
-                  })
-                  .catch(err => {
-                      console.log(err, 'No Response from Node JS');
-                  });
-              this.setState({result:r});
-              console.log(this.state.result);*/
-
+             axios.get('http://localhost:3001/', {
+                 params: {number1: n1, number2: n2, operator: op}
+             })
+                 .then(response => {
+                     r = response.data.result;
+                     console.log(r, 'from Node jS!' + this.state.operator + "operator");
+                     console.log(r+"r : ")
+                     this.state.result = r;
+                     this.setState({result: this.state.result});
+                     console.log(this.state.result +"this.state.result");
+                     document.getElementById("textbox1").value = this.state.result;
+                     this.state.displayValue = this.state.result;
+                     this.setState({
+                         displayValue: this.state.result
+                     });
+                 })
+                 .catch(err => {
+                     console.log(err, 'No Response from Node JS');
+                 });
     }
     render() {
         return (
@@ -73,7 +148,7 @@ class App extends React.Component {
                             <div className="row align-items-center">
                                 <div className="row">
                                     <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <input id="textbox1" type="text" defaultValue="0"/>
+                                        <input id="textbox1" type="text" value={this.state.displayValue} onChange={this.handleChange}/>
                                     </div>
                                 </div>
                                 <div className="row align-items-center">
@@ -84,34 +159,34 @@ class App extends React.Component {
                                 </div>
                                 <div className="row">
                                     <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btn7" value="7" className="btn btn-default" onClick={this.numberClicked(7)}>7</button>
-                                        <button id="btn8" value="8" className="btn btn-default" onClick={this.numberClicked(8)}>8</button>
-                                        <button id="btn9" value="9" className="btn btn-default" onClick={this.numberClicked(9)}>9</button>
-                                        <button id="btnDiv" value="/" className="btn btn-default" onClick={this.opClicked('/')}>/</button>
+                                        <button id="btn7" value="7" className="btn btn-default" onClick={this.numberClicked}>7</button>
+                                        <button id="btn8" value="8" className="btn btn-default" onClick={this.numberClicked}>8</button>
+                                        <button id="btn9" value="9" className="btn btn-default" onClick={this.numberClicked}>9</button>
+                                        <button id="btnDiv" value="/" className="btn btn-default" onClick={this.opClicked}>/</button>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btn4" value="4" className="btn btn-default" onClick={this.numberClicked(4)}>4</button>
-                                        <button id="btn5" value="5" className="btn btn-default" onClick={this.numberClicked(5)}>5</button>
-                                        <button id="btn6" value="6" className="btn btn-default" onClick={this.numberClicked(6)}>6</button>
-                                        <button id="btnMult" value="*" className="btn btn-default" onClick={this.opClicked('*')}>*</button>
+                                        <button id="btn4" value="4" className="btn btn-default" onClick={this.numberClicked}>4</button>
+                                        <button id="btn5" value="5" className="btn btn-default" onClick={this.numberClicked}>5</button>
+                                        <button id="btn6" value="6" className="btn btn-default" onClick={this.numberClicked}>6</button>
+                                        <button id="btnMult" value="*" className="btn btn-default" onClick={this.opClicked}>*</button>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btn1" value="1" className="btn btn-default" onClick={this.numberClicked(1)}>1</button>
-                                        <button id="btn2" value="2" className="btn btn-default" onClick={this.numberClicked(2)}>2</button>
-                                        <button id="btn3" value="3" className="btn btn-default" onClick={this.numberClicked(3)}>3</button>
-                                        <button id="btnSub" value="-" className="btn btn-default" onClick={this.opClicked('-')}>-</button>
+                                        <button id="btn1" value="1" className="btn btn-default" onClick={this.numberClicked}>1</button>
+                                        <button id="btn2" value="2" className="btn btn-default" onClick={this.numberClicked}>2</button>
+                                        <button id="btn3" value="3" className="btn btn-default" onClick={this.numberClicked}>3</button>
+                                        <button id="btnSub" value="-" className="btn btn-default" onClick={this.opClicked}>-</button>
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btn0" className="btn btn-default" onClick={this.numberClicked(0)}>0</button>
-                                        <button id="btnPt" className="btn btn-default" onClick={this.opClicked('.')}>.</button>
-                                        <button id="btnAdd"className="btn btn-default" onClick={this.opClicked('+')}>+</button>
-                                        <button id="btnEql" className="btn btn-default" onClick={this.opClicked('=')}>=</button>
+                                        <button id="btn0" value="0" className="btn btn-default" onClick={this.numberClicked}>0</button>
+                                        <button id="btnPt" value="." className="btn btn-default" onClick={this.opClicked}>.</button>
+                                        <button id="btnAdd" value="+" className="btn btn-default" onClick={this.opClicked}>+</button>
+                                        <button id="btnEql" value="=" className="btn btn-default" onClick={this.opClicked}>=</button>
                                     </div>
                                 </div>
                             </div>
