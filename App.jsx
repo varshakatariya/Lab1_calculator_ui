fbe18af68bbf78175e9a6b24ca072b8c4c3eabe3
Varsha Amrutlal Katariya 012468079 <varshaamrutlal.katariya@sjsu.edu>
2018-03-14 16:14:36 -0700

    Added clearing and decimal point to calculator and also cleaned up for console logs

--- App.jsx
+++ App.jsx
@@ -4,15 +4,6 @@ import axios from 'axios';
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.initialize();
-        this.calculate = this.calculate.bind(this);
-        this.clear = this.clear.bind(this);
-        this.numberClicked = this.numberClicked.bind(this);
-        this.opClicked = this.opClicked.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-    };
-
-    initialize(){
         this.state = {
             numberClicked:0,
             number:[],
@@ -23,30 +14,42 @@ class App extends React.Component {
             sign:'',
             displayValue: 0
         }
-    }
+        this.calculate = this.calculate.bind(this);
+        this.clear = this.clear.bind(this);
+        this.numberClicked = this.numberClicked.bind(this);
+        this.opClicked = this.opClicked.bind(this);
+        this.pointClicked = this.pointClicked.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    };
 
     handleChange(event) {
         this.state.displayValue = event.target.value;
         this.setState({displayValue: this.state.displayValue});
     }
-
-    clear(){
-        this.initialize();
-    }
-
-    changeSign(){
-
+    clear() {
+        document.getElementById("textbox1").value = 0;
+        this.setState({
+            numberClicked:0,
+            number:[],
+            firstNumber:0,
+            secondNumber:0,
+            operator:'',
+            result: 0,
+            sign:'',
+            displayValue: 0
+        });
     }
 
     setFirstNumber(first) {
         if (this.state.firstNumber == 0) {
-            this.initialize();
             this.state.firstNumber = first;
         }
         else {
             first = this.state.firstNumber + "" + first;
         }
-        console.log("state first number : "+this.state.firstNumber);
+        if(first == "."){
+            first = this.state.firstNumber + "" + first;
+        }
         this.state.firstNumber = first;
         this.setState({
             firstNumber: this.state.firstNumber
@@ -65,8 +68,9 @@ class App extends React.Component {
         else {
             sec = this.state.secondNumber + "" + sec;
         }
-        console.log("state first number : "+this.state.secondNumber);
-
+        if(sec == "."){
+            first = this.state.secondNumber + "" + sec;
+        }
         this.state.secondNumber = sec;
         this.setState({
             secondNumber: this.state.secondNumber
@@ -80,6 +84,15 @@ class App extends React.Component {
 
     numberClicked(event){
 
+        if(this.state.operator == ""){
+            this.setFirstNumber(event.target.value);
+        }
+        else{
+            this.setSecondNumber(event.target.value);
+        }
+    }
+
+    pointClicked(event){
         if(this.state.operator == ""){
             this.setFirstNumber(event.target.value);
         }
@@ -110,90 +123,89 @@ class App extends React.Component {
     }
 
     calculate(){
-        console.log(this.state.operator);
+        var n1 = Number(this.state.firstNumber);
+        var n2 = Number(this.state.secondNumber);
+        var r = 0;
+        var op = this.state.operator;
 
-          var n1 = Number(this.state.firstNumber);
-          var n2 = Number(this.state.secondNumber);
-          var r = 0;
-          var op = this.state.operator;
-
-         console.log(this.state.operator);
-
-             axios.get('http://localhost:3001/', {
-                 params: {number1: n1, number2: n2, operator: op}
-             })
-                 .then(response => {
-                     r = response.data.result;
-                     console.log(r, 'from Node jS!' + this.state.operator + "operator");
-                     console.log(r+"r : ")
-                     this.state.result = r;
-                     this.setState({result: this.state.result});
-                     console.log(this.state.result +"this.state.result");
-                     document.getElementById("textbox1").value = this.state.result;
-                     this.state.displayValue = this.state.result;
-                     this.setState({
-                         displayValue: this.state.result
-                     });
-                 })
-                 .catch(err => {
-                     console.log(err, 'No Response from Node JS');
-                 });
+        axios.get('http://localhost:3001/', {
+            params: {number1: n1, number2: n2, operator: op}
+        })
+            .then(response => {
+                r = response.data.result;
+                this.state.result = r;
+                this.setState({result: this.state.result});
+                this.setState({
+                    numberClicked:0,
+                    number:[],
+                    firstNumber:0,
+                    secondNumber:0,
+                    operator:'',
+                    result: this.state.result,
+                    sign:'',
+                    displayValue: 0
+                });
+                console.log(this.state.result +" = result");
+                document.getElementById("textbox1").value = this.state.result;
+                this.state.displayValue = this.state.result;
+                this.setState({
+                    displayValue: this.state.result
+                });
+            })
+            .catch(err => {
+                console.log(err, 'No Response from Node JS');
+            });
     }
     render() {
         return (
-                <div className="container">
-                    <br/><br/><br/><br/>
-                    <div className="form-group">
-                        <div className="well table-bordered">
-                            <div className="row align-items-center">
-                                <div className="row">
-                                    <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <input id="textbox1" type="text" value={this.state.displayValue} onChange={this.handleChange}/>
-                                    </div>
-                                </div>
-                                <div className="row align-items-center">
-                                    <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btnClr" value="C" className="btn btn-default" type="button" onClick={this.clear}> C </button>
-                                        <button id="btnSign" value="+-" className="btn btn-default" type="button" onClick={this.changeSign('-')}> +- </button>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btn7" value="7" className="btn btn-default" onClick={this.numberClicked}>7</button>
-                                        <button id="btn8" value="8" className="btn btn-default" onClick={this.numberClicked}>8</button>
-                                        <button id="btn9" value="9" className="btn btn-default" onClick={this.numberClicked}>9</button>
-                                        <button id="btnDiv" value="/" className="btn btn-default" onClick={this.opClicked}>/</button>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btn4" value="4" className="btn btn-default" onClick={this.numberClicked}>4</button>
-                                        <button id="btn5" value="5" className="btn btn-default" onClick={this.numberClicked}>5</button>
-                                        <button id="btn6" value="6" className="btn btn-default" onClick={this.numberClicked}>6</button>
-                                        <button id="btnMult" value="*" className="btn btn-default" onClick={this.opClicked}>*</button>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btn1" value="1" className="btn btn-default" onClick={this.numberClicked}>1</button>
-                                        <button id="btn2" value="2" className="btn btn-default" onClick={this.numberClicked}>2</button>
-                                        <button id="btn3" value="3" className="btn btn-default" onClick={this.numberClicked}>3</button>
-                                        <button id="btnSub" value="-" className="btn btn-default" onClick={this.opClicked}>-</button>
-                                    </div>
-                                </div>
-                                <div className="row">
-                                    <div className="cols-sm-10 col-xs-10 col-md-10 col-sm-offset-4">
-                                        <button id="btn0" value="0" className="btn btn-default" onClick={this.numberClicked}>0</button>
-                                        <button id="btnPt" value="." className="btn btn-default" onClick={this.opClicked}>.</button>
-                                        <button id="btnAdd" value="+" className="btn btn-default" onClick={this.opClicked}>+</button>
-                                        <button id="btnEql" value="=" className="btn btn-default" onClick={this.opClicked}>=</button>
-                                    </div>
-                                </div>
-                            </div>
+            <div className="container align-content-center">
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <form className="form-inline">
+                    <div className="row">
+                        <div className="col-sm-8 col-sm-offset-4">
+                            <input className="col-xs-4" id="textbox1" type="text" value={this.state.displayValue} onChange={this.handleChange}/>
                         </div>
                     </div>
+                <div className="row">
+                    <div className="col-sm-8 col-sm-offset-4">
+                            <button id="btnClr" value="C" className="btn btn-default col-xs-2" type="button" onClick={this.clear}> C </button>
+                            <button id="btnSign" value="-" className="btn btn-default col-xs-2" type="button" > +- </button>
+                        </div>
                 </div>
-
+                <div className="row">
+                    <div className="col-sm-8 col-sm-offset-4">
+                            <button id="btn7" value="7" className="btn btn-default col-xs-1" onClick={this.numberClicked}>7</button>
+                            <button id="btn8" value="8" className="btn btn-default col-xs-1" onClick={this.numberClicked}>8</button>
+                            <button id="btn9" value="9" className="btn btn-default col-xs-1" onClick={this.numberClicked}>9</button>
+                            <button id="btnDiv" value="/" className="btn btn-default col-xs-1" onClick={this.opClicked}>/</button>
+                        </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-8 col-sm-offset-4">
+                            <button id="btn4" value="4" className="btn btn-default col-xs-1" onClick={this.numberClicked}>4</button>
+                            <button id="btn5" value="5" className="btn btn-default col-xs-1" onClick={this.numberClicked}>5</button>
+                            <button id="btn6" value="6" className="btn btn-default col-xs-1" onClick={this.numberClicked}>6</button>
+                            <button id="btnMult" value="*" className="btn btn-default col-xs-1" onClick={this.opClicked}>*</button>
+                        </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-8 col-sm-offset-4">
+                            <button id="btn1" value="1" className="btn btn-default col-xs-1" onClick={this.numberClicked}>1</button>
+                            <button id="btn2" value="2" className="btn btn-default col-xs-1" onClick={this.numberClicked}>2</button>
+                            <button id="btn3" value="3" className="btn btn-default col-xs-1" onClick={this.numberClicked}>3</button>
+                            <button id="btnSub" value="-" className="btn btn-default col-xs-1" onClick={this.opClicked}>-</button>
+                        </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-8 col-sm-offset-4">
+                            <button id="btn0" value="0" className="btn btn-default col-xs-1" onClick={this.numberClicked}>0</button>
+                            <button id="btnPt" value="." className="btn btn-default col-xs-1" onClick={this.pointClicked}>.</button>
+                            <button id="btnAdd" value="+" className="btn btn-default col-xs-1" onClick={this.opClicked}>+</button>
+                            <button id="btnEql" value="=" className="btn btn-default col-xs-1" onClick={this.opClicked}>=</button>
+                        </div>
+                </div>
+                </form>
+            </div>
         );
     }
 }
